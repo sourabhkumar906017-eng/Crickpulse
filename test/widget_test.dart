91a1742eb58394047b74a5dbacac7c90f93a451f
sourabhkumar906017-eng <sourabhkumar906017@gmail.com>
2025-11-13 18:59:36 +0530

    Add files via upload

---
+++ test/widget_test.dart
@@ -0,0 +1,24 @@
+// import 'package:flutter/material.dart';
+// import 'package:flutter_test/flutter_test.dart';
+//
+// // Correct import with your actual package name
+// import 'package:crickpulse/main.dart';
+//
+// void main() {
+//   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+//     // Build our app and trigger a frame.
+//     await tester.pumpWidget(const CrickPulse()); // use your main widget class
+//
+//     // Verify that our counter starts at 0.
+//     expect(find.text('0'), findsOneWidget);
+//     expect(find.text('1'), findsNothing);
+//
+//     // Tap the '+' icon and trigger a frame.
+//     await tester.tap(find.byIcon(Icons.add));
+//     await tester.pump();
+//
+//     // Verify that our counter has incremented.
+//     expect(find.text('0'), findsNothing);
+//     expect(find.text('1'), findsOneWidget);
+//   });
+// }
